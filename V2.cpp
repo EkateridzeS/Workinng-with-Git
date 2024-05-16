760950ea933934ce05dc22bdd3f19901a3d26819
Zelenova Ekaterina <ezelenova08@yandex.ru>
2024-05-16 13:13:34 +0400

    Дана последовательность целых чисел. Создать новую последовательность, удалив все нечетные числа.

---
+++ V2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+using namespace std;
+
+int X;
+bool kratn(int x) {
+    return x % 2 != 0;
+}
+
+int main() {
+    int n;
+    cout << "n = "; cin >> n;
+    vector<int>vec;
+    int x;
+    for (int i = 0;i < n;i++) {
+        cout << "a[" << i << "]=";
+        cin >> x;
+        vec.push_back(x);
+    }
+
+    for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++)
+        cout << *iter << " ";
+    cout << endl;
+
+    vector<int>::iterator it = remove_if(vec.begin(), vec.end(), kratn);
+    vec.erase(it, vec.end());
+
+
+    vector<int> temp(vec);
+    int MIN = *min_element(vec.begin(), vec.end());
+    replace_if(vec.begin(),vec.end(),kratn,MIN);
+
+    for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++)
+    cout << *iter << " ";
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
